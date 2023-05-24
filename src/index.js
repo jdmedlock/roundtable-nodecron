47b4eb05c71bed75fae25365b027013f95272745
jdmedlock <jdmedlock@gmail.com>
2023-05-24 14:38:25 -0500

    Feature: Format datetime output

--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import * as dotenv from 'dotenv'
 
 dotenv.config()
 
+const sleep = (secondsToSleep) => {
+  if (secondsToSleep === 0) {
+    return new Promise(resolve => null)
+  }
+  return new Promise(resolve => setTimeout(resolve, secondsToSleep*1000))
+}
+
 const displayGreeting = () => {
-  console.log(`Hello world @ `)
+  const currentDate = new Date()
+  const displayDateTime = new Intl.DateTimeFormat('en-US', {
+    dateStyle: 'full',
+    timeStyle: 'long',
+    timeZone: 'America/Chicago'
+  }).format(currentDate)
+  console.log('Hello world @', displayDateTime)
 }
 
 // Run the scheduled task
-cron.schedule(`*/1 * * * * *`, async () => { // Test to run once every minute
+cron.schedule(`* */1 * * * *`, async () => { // Test to run once every minute
   displayGreeting()
 })
