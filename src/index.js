65bb360828803d30d3db1ed72bbb484a7da9bc19
jdmedlock <jdmedlock@gmail.com>
2023-05-27 09:52:09 -0500

    Refactor: Include more exampes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const sleep = async (secondsToSleep) => {
 }
 
 const displayGreeting = async () => {
+  await sleep(2)
   const currentDate = new Date()
   const displayDateTime = new Intl.DateTimeFormat('en-US', {
     dateStyle: 'full',
@@ -15,12 +16,22 @@ const displayGreeting = async () => {
 }
 
 // Run the scheduled task
-cron.schedule(`0-59 * * * * *`, async () => { // Test to run every minute at second 5-40
+//cron.schedule(`5-40 * * * * *`, async () => { // Test to run every minute at second 5-40
+//cron.schedule(`10,20,30 * * * * *`, async () => { // Test to run every 10 seconds
+cron.schedule(`5-40 * * * * *`, async () => { // Test to run every other minute
   displayGreeting()
 })
 
+console.log('task is scheduled')
+/*
+sleep(25).then(() => {
+  cron.stop() // TODO: Research why this fails
+  console.log('task is stopped')
+})
+*/
+
 /*
 for (let i = 0; i < 10000; ++i) {
-  console.log(`Hi Regina ${ i }`)
+  console.log(`Hi Sandramsc ${ i }`)
 }
 */
